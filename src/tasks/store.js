ac6de51ddfaeb197ce30eeba6cba01466a915493
Brendan Cordova <cordovalegacy19@gmail.com>
2023-07-27 09:46:37 -0700

    Redux Core Done

--- src/tasks/store.js
+++ src/tasks/store.js
@@ -3,7 +3,10 @@ import { legacy_createStore as createStore, applyMiddleware } from 'redux'
 import reducer from './reducer'
 import thunk from 'redux-thunk'
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(
+    reducer,
+    applyMiddleware(thunk)
+)
 
 export default store
 
